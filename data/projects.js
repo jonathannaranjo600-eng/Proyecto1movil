567205836482c6ac89f5d125eeb1dc640526923a
jonathan <jonathannaranjo600@gmail.com>
2026-08-13 10:18:20 -0600

    feat: agrego datos, ProjectCard y ProjectsScreen con FlatList

---
+++ data/projects.js
@@ -0,0 +1,33 @@
+
+const projects = [
+  {
+    id: '1',
+    titulo: 'App de Educación Kuvo',
+    imagen: 'https://picsum.photos/id/20/400/250',
+    descripcionCorta: 'App de educacion para profesores y alumnos',
+    descripcionLarga:
+      'Es un ecosistema donde los profesores pueden crear cursos y los alumnos pueden inscribirse a ellos, con la posibilidad de realizar evaluaciones y seguimiento del progreso.',
+    tecnologias: ['Java', 'CSS', 'JavaScript'],
+  },
+  {
+    id: '2',
+    titulo: 'Portafolio Web',
+    imagen: 'https://picsum.photos/id/180/400/250',
+    descripcionCorta: 'Sitio web personal responsivo.',
+    descripcionLarga:
+      'Página web personal construida con HTML, CSS y JavaScript, totalmente adaptable a móviles y con formulario de contacto.',
+    tecnologias: ['HTML', 'CSS', 'JavaScript'],
+  },
+  {
+    id: '3',
+    titulo: 'ncode digital',
+    imagen: 'https://picsum.photos/id/48/400/250',
+    descripcionCorta: 'Sitio web especializado en web y diseños gráfico.',
+    descripcionLarga:
+      'Una plataforma para la creación y gestión de sitios web y diseños gráficos.',
+    tecnologias: ['HTML', 'Figma', 'JavaScript'],
+  },
+];
+
+
+export default projects;
